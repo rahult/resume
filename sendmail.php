6dbe04dee8dfdbcef32c521cb05776525e0cf04f
Rahul Trikha <rahul.trikha@gmail.com>
2011-06-02 00:46:49 +0930

    Updated Send Mail

--- sendmail.php
+++ sendmail.php
@@ -1,16 +1,16 @@
 <?php
 /*
 
-This script was created by Bryan Helmig at midmodesign.com. It is licensed under http://creativecommons.org/licenses/by-nc-sa/3.0/us/. 
+This script was created by Bryan Helmig at midmodesign.com. It is licensed under http://creativecommons.org/licenses/by-nc-sa/3.0/us/.
 1)   A quick primer: place this on your server. Create a form like below:
 --------------------------------------------------------------------------------------------------------
 <form action="sendmail.php" method="post" id="contactForm">
-	<p>Name:</p> <input type="text" name="name" value="" id="name" />
-	<p>Email:</p> <input type="text" name="email" value="" id="email" />
-	<p>Telephone:</p> <input type="text" name="tele" value="" id="tele" />
-	<span style="display:none;"><p>Honeypot:</p> <input type="text" name="last" value="" id="last" /></span>
-	<p>Message:</p> <textarea rows="5" name="message"></textarea>
-	<input type="submit" value="Send Message" />
+  <p>Name:</p> <input type="text" name="name" value="" id="name" />
+  <p>Email:</p> <input type="text" name="email" value="" id="email" />
+  <p>Telephone:</p> <input type="text" name="tele" value="" id="tele" />
+  <span style="display:none;"><p>Honeypot:</p> <input type="text" name="last" value="" id="last" /></span>
+  <p>Message:</p> <textarea rows="5" name="message"></textarea>
+  <input type="submit" value="Send Message" />
 </form
 --------------------------------------------------------------------------------------------------------
 2)   This will work fine for a standard form. If you want ajax power, add this div above or below and hide it with css.
@@ -22,44 +22,44 @@ This script was created by Bryan Helmig at midmodesign.com. It is licensed under
 <script type="text/javascript" src="$-latest.pack.js"></script>
 <script type="text/javascript" src="$.form.js"></script>
 <script type="text/javascript">
-$(document).ready(function() { 
-var options = { 
+$(document).ready(function() {
+var options = {
 target:        '#alert',
 beforeSubmit:  showRequest,
 success:       showResponse
-}; 
-$('#contactForm').ajaxForm(options); 
-}); 
-function showRequest(formData, jqForm, options) { 
-var queryString = $.param(formData); 
-return true; 
-} 
-function showResponse(responseText, statusText)  {  
-} 
+};
+$('#contactForm').ajaxForm(options);
+});
+function showRequest(formData, jqForm, options) {
+var queryString = $.param(formData);
+return true;
+}
+function showResponse(responseText, statusText)  {
+}
 $.fn.clearForm = function() {
   return this.each(function() {
-	var type = this.type, tag = this.tagName.toLowerCase();
-	if (tag == 'form')
-	  return $(':input',this).clearForm();
-	if (type == 'text' || type == 'password' || tag == 'textarea')
-	  this.value = '';
-	else if (type == 'checkbox' || type == 'radio')
-	  this.checked = false;
-	else if (tag == 'select')
-	  this.selectedIndex = -1;
+  var type = this.type, tag = this.tagName.toLowerCase();
+  if (tag == 'form')
+    return $(':input',this).clearForm();
+  if (type == 'text' || type == 'password' || tag == 'textarea')
+    this.value = '';
+  else if (type == 'checkbox' || type == 'radio')
+    this.checked = false;
+  else if (tag == 'select')
+    this.selectedIndex = -1;
   });
 };
 </script>
 --------------------------------------------------------------------------------------------------------
 
-Boom. There it is. 
+Boom. There it is.
 */
 
 //        Who you want to recieve the emails from the form. (Hint: generally you.)
-$sendto = 'you@yourdomain.com';
+$sendto = 'contact@rahultrikha.com';
 
 //        The subject you'll see in your inbox
-$subject = 'A message from your website';
+$subject = 'A message from your rahultrikha.com';
 
 //        Message for the user when he/she doesn't fill in the form correctly.
 $errormessage = 'Looks like you\'re missing some info. Try again.';
@@ -97,74 +97,74 @@ function clean_var($variable) {
 //The first if for honeypot.
 if ( empty($_REQUEST['last']) ) {
 
-	// A bunch of if's for all the fields and the error messages.
-	if ( empty($_REQUEST['name']) ) {
-		$pass = 1;
-		//$alert .= "<li>" . $emptyname . "</li>";
-		$alert .= "<script>jQuery(\"#name\").addClass(\"error\");</script>";
-	} elseif ( ereg( "[][{}()*+?.\\^$|]", $_REQUEST['name'] ) ) {
-		$pass = 1;
-		//$alert .= "<li>" . $alertname . "</li>";
-	}
-	if ( empty($_REQUEST['email']) ) {
-		$pass = 1;
-		//$alert .= "<li>" . $emptyemail . "</li>";
-		$alert .= "<script>jQuery(\"#email\").addClass(\"error\");</script>";
-	} elseif ( !eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $_REQUEST['email']) ) {
-		$pass = 1;
-		//$alert .= "<li>" . $alertemail . "</li>";
-	}
-	/*if ( empty($_REQUEST['tele']) ) {
-		$pass = 1;
-		//$alert .= "<li>" . $emptytele . "</li>";
-		$alert .= "<script>jQuery(\"#tele\").addClass(\"error\");</script>";
-	} elseif ( !ereg( "\(?[0-9]{3}\)?[-. ]?[0-9]{3}[-. ]?[0-9]{4}", $_REQUEST['tele'] ) ) {
-		$pass = 1;
-		$alert .= "<li>" . $alerttele . "</li>";
-	}*/
-	if ( empty($_REQUEST['message']) ) {
-		$pass = 1;
-		//$alert .= "<li>" . $emptymessage . "</li>";
-		$alert .= "<script>jQuery(\"#message\").addClass(\"error\");</script>";
-	} elseif ( ereg( "[][{}()*+?\\^$|]", $_REQUEST['message'] ) ) {
-		$pass = 1;
-		//$alert .= "<li>" . $alertmessage . "</li>";
-	}
+  // A bunch of if's for all the fields and the error messages.
+  if ( empty($_REQUEST['name']) ) {
+    $pass = 1;
+    //$alert .= "<li>" . $emptyname . "</li>";
+    $alert .= "<script>jQuery(\"#name\").addClass(\"error\");</script>";
+  } elseif ( ereg( "[][{}()*+?.\\^$|]", $_REQUEST['name'] ) ) {
+    $pass = 1;
+    //$alert .= "<li>" . $alertname . "</li>";
+  }
+  if ( empty($_REQUEST['email']) ) {
+    $pass = 1;
+    //$alert .= "<li>" . $emptyemail . "</li>";
+    $alert .= "<script>jQuery(\"#email\").addClass(\"error\");</script>";
+  } elseif ( !eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $_REQUEST['email']) ) {
+    $pass = 1;
+    //$alert .= "<li>" . $alertemail . "</li>";
+  }
+  /*if ( empty($_REQUEST['tele']) ) {
+    $pass = 1;
+    //$alert .= "<li>" . $emptytele . "</li>";
+    $alert .= "<script>jQuery(\"#tele\").addClass(\"error\");</script>";
+  } elseif ( !ereg( "\(?[0-9]{3}\)?[-. ]?[0-9]{3}[-. ]?[0-9]{4}", $_REQUEST['tele'] ) ) {
+    $pass = 1;
+    $alert .= "<li>" . $alerttele . "</li>";
+  }*/
+  if ( empty($_REQUEST['message']) ) {
+    $pass = 1;
+    //$alert .= "<li>" . $emptymessage . "</li>";
+    $alert .= "<script>jQuery(\"#message\").addClass(\"error\");</script>";
+  } elseif ( ereg( "[][{}()*+?\\^$|]", $_REQUEST['message'] ) ) {
+    $pass = 1;
+    //$alert .= "<li>" . $alertmessage . "</li>";
+  }
 
-	//If the user err'd, print the error messages.
-	if ( $pass==1 ) {
+  //If the user err'd, print the error messages.
+  if ( $pass==1 ) {
 
-	//This first line is for ajax/javascript, comment it or delete it if this isn't your cup o' tea.
-	//echo "<script>$(\".message\").hide(\"slow\").show(\"slow\"); </script>";
-	echo $errormessage;
-	echo $alert;
+  //This first line is for ajax/javascript, comment it or delete it if this isn't your cup o' tea.
+  //echo "<script>$(\".message\").hide(\"slow\").show(\"slow\"); </script>";
+  echo $errormessage;
+  echo $alert;
 
-	// If the user didn't err and there is in fact a message, time to email it.
-	} elseif (isset($_REQUEST['message'])) {
-	    
-		//Construct the message.
-	    $message = "From: " . clean_var($_REQUEST['name']) . "\n";
-		$message .= "Email: " . clean_var($_REQUEST['email']) . "\n";
-	    //$message .= "Telephone: " . clean_var($_REQUEST['tele']) . "\n";
-	    $message .= "Message: \n" . clean_var($_REQUEST['message']);
-	    $header = 'From:'. clean_var($_REQUEST['email']);
-	    
-	//Mail the message - for production
-		mail($sendto, $subject, $message, $header);
-	//This is for javascript, 
-		echo "<script>jQuery(':input').clearForm();</script>";
-		//echo $thanks;
-		echo "<script>jQuery(\"#name\").removeClass(\"error\");jQuery(\"#email\").removeClass(\"error\");jQuery(\"#tele\").removeClass(\"error\");jQuery(\"#message\").removeClass(\"error\");</script>";
-		die();
+  // If the user didn't err and there is in fact a message, time to email it.
+  } elseif (isset($_REQUEST['message'])) {
+
+    //Construct the message.
+      $message = "From: " . clean_var($_REQUEST['name']) . "\n";
+    $message .= "Email: " . clean_var($_REQUEST['email']) . "\n";
+      //$message .= "Telephone: " . clean_var($_REQUEST['tele']) . "\n";
+      $message .= "Message: \n" . clean_var($_REQUEST['message']);
+      $header = 'From:'. clean_var($_REQUEST['email']);
+
+  //Mail the message - for production
+    mail($sendto, $subject, $message, $header);
+  //This is for javascript,
+    echo "<script>jQuery(':input').clearForm();</script>";
+    //echo $thanks;
+    echo "<script>jQuery(\"#name\").removeClass(\"error\");jQuery(\"#email\").removeClass(\"error\");jQuery(\"#tele\").removeClass(\"error\");jQuery(\"#message\").removeClass(\"error\");</script>";
+    die();
 
 //Echo the email message - for development
-		//echo "<br/><br/>" . $message;
+    //echo "<br/><br/>" . $message;
 
-	}
-	
+  }
+
 //If honeypot is filled, trigger the message that bot likely won't see.
 } else {
-	echo "<script>$(\".message\").hide(\"slow\").show(\"slow\"); </script>";
-	echo $honeypot;
+  echo "<script>$(\".message\").hide(\"slow\").show(\"slow\"); </script>";
+  echo $honeypot;
 }
 ?>
